35498c7c42d3a115d60b88b271e7cc4db8d10a34
steven victor <chikodi543@gmail.com>
2019-06-10 09:31:17 +0100

    JSON marshal and unmarshal

--- JSON_Marshal/main.go
+++ JSON_Marshal/main.go
@@ -11,6 +11,12 @@ type person struct {
 	Age   int
 }
 
+type person2 struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
+
 func main() {
 	p1 := person{
 		First: "Mike",
@@ -32,5 +38,29 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(bs))
+	fmt.Println("This is the Marshed byte: ", string(bs))
+
+	s := `[{"First":"Mike","Last":"Segun","Age":54},{"First":"Ade","Last":"Sally","Age":39}]`
+	fmt.Printf("%T\n", s)
+	bsNew := []byte(s)
+	fmt.Printf("%T\n", bsNew)
+	fmt.Println(bsNew)
+	fmt.Println()
+
+	var people2 []person2
+	// people2 := []person2{}
+
+	err2 := json.Unmarshal(bsNew, &people2)
+
+	if err != nil {
+		fmt.Println("This is the err: ", err2)
+	}
+
+	fmt.Println()
+	fmt.Println("All the data", people2)
+
+	for i, v := range people2 {
+		fmt.Println("\nPerson Number", i)
+		fmt.Println(v.First, v.Last, v.Age)
+	}
 }
